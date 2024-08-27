6be1c90252cd6dada6067adf85f3a435eb730161
stbow <sab805@gmail.com>
2024-08-27 15:37:01 -0700

    initial commit for todo-list

---
+++ script.js
@@ -0,0 +1,24 @@
+const tasks = [];
+const today = false;
+
+function showTasks() {
+  /* if today, for loop to show all tasks in today section
+  else show in tomorrow section */
+}
+
+function newTask() {
+  /* add to end of array (push, i think) */
+}
+
+function newDay() {
+  /* move all tasks to Today status, run showTasks */
+  /* also need to remove all tasks with .done from the array */
+}
+
+function checkOff() {
+  /* button to check off task - set .done class */
+}
+
+function delay () {
+  /* button for this - set to .tomorrow class, regenerate lists */
+}
